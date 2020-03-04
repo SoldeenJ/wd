2f2b0fc8e8f42e075e99852d182cea52b6a0b6d2
jc <ssoldeen@gmail.com>
2020-03-04 18:43:21 +0100

    a

---
+++ l2.py
@@ -0,0 +1,69 @@
+
+# zad 1
+
+# a = input("napisz zdanie")
+# print(a.count(" "))
+
+#zad 2
+
+# a = input("watrosc pierwsza")
+# b = input("wartosc druga")
+# c = int(a) * int(b)
+# print(c)
+
+# zad 4
+
+# a = input("podaj liczbe")
+# b = int(a)
+# if b < 0:
+#     print(-b)
+# else:
+#     print(b)
+
+# zad 5
+
+# a = int(input("podaj liczbe "))
+# b = int(input("podaj liczbe 2 "))
+# c = int(input("podaj liczbe 3 "))
+
+# if 0 < a < 10 and a>b or b>c:
+#     print("ok")
+# else:
+#     print("not ok")
+
+#zad 6
+
+# for x in range(0,1000,5):
+#     print(x)
+
+#zad 7
+
+# a = input("podaj liczby ")
+# lista1 = []
+# for i in range(len(a)):
+#     lista1.append(int(a[i]))
+# print(lista1)
+# for x in range(len(lista1)):
+#     print(lista1[x]*lista1[x])
+#idk, dokonczyc
+
+#zad8
+
+# a = int(input("podaj ilosc liczb "))
+# lista = []
+# x=0
+# while(x!=a):
+#     lista.append(input("podaj 1 liczbe"))
+#     x+=1
+# else:
+#     print(lista)
+
+#zad 9 
+
+
+
+
+
+
+
+
